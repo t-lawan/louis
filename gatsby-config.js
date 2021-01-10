f447dbc77da0799f11058cc617d8d42df1a7f364
akinsola <thomas.lawanson@gmail.com>
2021-01-10 20:54:57 +0000

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,30 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    // {
+    //   resolve: `gatsby-source-contentful`,
+    //   options: {
+    //     spaceId: `your_space_id`,
+    //     // Learn about environment variables: https://gatsby.dev/env-vars
+    //     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    //     downloadLocal: true,
+    //   },
+    // },
+    {
+      resolve: `gatsby-remark-images-contentful`,
+      options: {
+        // It's important to specify the maxWidth (in pixels) of
+        // the content container as this plugin uses this as the
+        // base for generating different widths of each image.
+        maxWidth: 590,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
---
+++ src/store/reducer.js
@@ -0,0 +1,60 @@
+import { createStore } from "redux"
+import * as ActionTypes from "./actions"
+const initalState = {
+  pages: [],
+  sidebar_links: [],
+  header_links: [],
+  navbar_links: [],
+  jumbotron_content: [],
+  modal_content: ModalTypes.CONTACT,
+  isLoaded: false,
+  show_mobile_modal: false,
+}
+
+const reducer = (state = initalState, action) => {
+  switch (action.type) {
+    case ActionTypes.SET_PAGES:
+      return Object.assign({}, state, {
+        pages: action.pages,
+      })
+    case ActionTypes.SET_SIDEBAR_LINKS:
+      return Object.assign({}, state, {
+        sidebar_links: action.sidebar_links,
+      })
+    case ActionTypes.SET_HEADER_LINKS:
+      return Object.assign({}, state, {
+        header_links: action.header_links,
+      })
+    case ActionTypes.SET_NAVBAR_LINKS:
+      return Object.assign({}, state, {
+        navbar_links: action.navbar_links,
+      })
+    case ActionTypes.SHOW_MOBILE_MODAL:
+      return Object.assign({}, state, {
+        show_mobile_modal: true,
+        modal_content: action.content
+      })
+    case ActionTypes.HIDE_MOBILE_MODAL:
+      return Object.assign({}, state, {
+        show_mobile_modal: false,
+        modal_content: ''
+      })
+    case ActionTypes.TOGGLE_MOBILE_MODAL:
+      return Object.assign({}, state, {
+        show_mobile_modal: !state.show_mobile_modal,
+        modal_content: action.content
+      })
+    case ActionTypes.IS_LOADED:
+      return Object.assign({}, state, {
+        isLoaded: true,
+      })
+    default:
+      return state
+  }
+}
+
+export const store = () =>
+  createStore(
+    reducer
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
